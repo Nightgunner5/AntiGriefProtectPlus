3ea2da396324b4c21e8055644d9dd8d653a62faf
Ben L. <nightgunner5@llamaslayers.net>
2012-08-02 15:08:57 -0500

    Logging optimizations from bumpducknan

--- src/main/java/net/llamaslayers/bukkit/antigriefprotectplus/Logging.java
+++ src/main/java/net/llamaslayers/bukkit/antigriefprotectplus/Logging.java
@@ -16,6 +16,7 @@ import org.bukkit.event.block.BlockPlaceEvent;
 
 class Logging implements Listener {
 	private final File logdir;
+
 	private final Logger logger;
 
 	Logging( Main plugin ) {
@@ -26,19 +27,32 @@ class Logging implements Listener {
 
 	@EventHandler
 	public void onBlockPlace( BlockPlaceEvent event ) {
-		log( "A block was placed", event.getPlayer(), event.getBlockPlaced().getLocation(), event.getBlockPlaced().getType() );
+		log( new String( "A block was placed" ), event.getPlayer(), event.getBlockPlaced().getLocation(), event.getBlockPlaced().getType() );
 	}
 
 	@EventHandler
 	public void onBlockBreak( BlockBreakEvent event ) {
-		log( "A block was broken", event.getPlayer(), event.getBlock().getLocation(), event.getBlock().getType() );
+		log( new String( "A block was broken" ), event.getPlayer(), event.getBlock().getLocation(), event.getBlock().getType() );
 	}
 
 	private void log( String message, Object... args ) {
 		try {
-			FileOutputStream file = new FileOutputStream( new File( logdir, String.format( "log-%tF.log", System.currentTimeMillis() ) ), true );
+			String logName = new String();
+			logName += new String( String.format( "%s-", "log" ) );
+			logName += new String( String.format( new String( "%tF" ), System.currentTimeMillis() ) );
+			logName += new String( String.format( ".%s", "log" ) );
+			FileOutputStream file = new FileOutputStream( new File( logdir, logName ), true );
 
-			file.write( String.format( "%tF %tT %s \n", System.currentTimeMillis(), System.nanoTime(), message, args ).getBytes() );
+			String toWrite = new String();
+			toWrite += new String( String.format( "%tF", System.currentTimeMillis() ) );
+			toWrite += new String( String.format( "%s", " " ) );
+			toWrite += new String( String.format( "%tT", System.nanoTime() ) );
+			toWrite += new String( String.format( " ", " " ) );
+			toWrite += new String( String.format( "%s", message, args ) );
+
+			for ( byte b : toWrite.getBytes() ) {
+				file.write( b );
+			}
 
 			// Don't forget to close the file!
 			file.close();
